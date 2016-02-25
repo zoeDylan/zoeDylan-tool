42ce5a588e65b5436a288cfe5719b837e81402f5
Moizd <zoedylan@live.com>
2016-02-25 10:34:41 +0800

    添加扩展功能

---
+++ statics/js/extend.js
@@ -0,0 +1,39 @@
+﻿/*
+ * 随机值
+ * min:最小值
+ * max:最大值
+ * randomNum(10,20);获取10到20的值
+ * randomNum(10);获取0-10的值
+ * randomNum(1.0,10);获取0-10的值(小数)
+ */
+function randomNum(min, max) {
+    if (typeof (max) == 'undefined') {
+        max = min;
+        min = 0;
+    }
+    return Math.random() * (max - min) + min;
+}
+
+/*
+ * 去字符串首尾空格
+ * 使用理由：性能优化惊人
+ * 代码来源：http://www.cnblogs.com/rubylouvre/archive/2009/09/18/1568794.html
+ * 描述：它先是把可能的空白符全部列出来，在第一次遍历中砍掉前面的空白，第二次砍掉后面的空白。全过程只用了indexOf与substring这个专门为处理字符串而生的原生方法，没有使用到正则。速度快得惊人，估计直逼上内部的二进制实现，并且在IE与火狐（其他浏览器当然也毫无疑问）都有良好的表现。速度都是零毫秒级别的。
+ */
+String.prototype.trim = function () {
+    var str = this,
+    whitespace = ' \n\r\t\f\x0b\xa0\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u200b\u2028\u2029\u3000';
+    for (var i = 0, len = str.length; i < len; i++) {
+        if (whitespace.indexOf(str.charAt(i)) === -1) {
+            str = str.substring(i);
+            break;
+        }
+    }
+    for (i = str.length - 1; i >= 0; i--) {
+        if (whitespace.indexOf(str.charAt(i)) === -1) {
+            str = str.substring(0, i + 1);
+            break;
+        }
+    }
+    return whitespace.indexOf(str.charAt(0)) === -1 ? str : '';
+}
